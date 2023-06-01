f1ecaa28d5570017fb7768c1216f44120fb182f2
mathias199 <mathiaschikhwaya@gmail.com>
2023-06-01 11:43:36 -0700

    all

--- 7-leet.c
+++ 7-leet.c
@@ -1,36 +1,25 @@
 #include "main.h"
 /**
- *leet - encodes a string into 1337.
- *letters a and A are replaced by 4.
- *Letters e and E are replaced by 3
- *Letters o and O are replaced by 0
- *Letters t and T are replaced by 7
- *Letters l and L are replaced by 1
- *@s: pointer to string.
- *
- *Return: pointer to s.
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
  */
-char *leet(char *s)
+char *leet(char *n)
 {
-	int stringCount, leetCount;
-	char leetLetters[] = "aAeEoOtTlL";
-	char leetNums[] = "4433007711";
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-/*  scan through string */
-	stringCount = 0;
-	while (s[stringCount] != '\0')
-/* check whether leetLetter is found */
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		leetCount = 0;
-		while (leetCount < 10)
+		for (j = 0; j < 10; j++)
 		{
-			if (leetLetters[leetCount] == s[stringCount])
+			if (n[i] == s1[j])
 			{
-				s[stringCount] = leetNums[leetCount];
+				n[i] = s2[j];
 			}
-			leetCount++;
 		}
-		stringCount++;
 	}
-	return (s);
+	return (n);
 }
+
